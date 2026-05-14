3cec2b095e8a6b572b209585eb64a7be3d600506
Muhammadjonov98 <muhammadjonov98@gmail.com>
2026-05-14 11:13:43 +0700

    add metered turn server

--- src/pages/Call.js
+++ src/pages/Call.js
@@ -188,17 +188,28 @@ function Call() {
     // Конфигурация с публичными STUN серверами Google
     const config = {
       iceServers: [
-        { urls: 'stun:stun.l.google.com:19302' },
-        { urls: 'stun:stun1.l.google.com:19302' },
         {
-          urls: 'turn:openrelay.metered.ca:80',
-          username: 'openrelayproject',
-          credential: 'openrelayproject',
+          urls: "stun:stun.relay.metered.ca:80",
         },
         {
-          urls: 'turn:openrelay.metered.ca:443',
-          username: 'openrelayproject',
-          credential: 'openrelayproject',
+          urls: "turn:global.relay.metered.ca:80",
+          username: "6f90f2d5ec8fafb71a757a0f",
+          credential: "b9MWx+jNYUHy0iDe",
+        },
+        {
+          urls: "turn:global.relay.metered.ca:80?transport=tcp",
+          username: "6f90f2d5ec8fafb71a757a0f",
+          credential: "b9MWx+jNYUHy0iDe",
+        },
+        {
+          urls: "turn:global.relay.metered.ca:443",
+          username: "6f90f2d5ec8fafb71a757a0f",
+          credential: "b9MWx+jNYUHy0iDe",
+        },
+        {
+          urls: "turns:global.relay.metered.ca:443?transport=tcp",
+          username: "6f90f2d5ec8fafb71a757a0f",
+          credential: "b9MWx+jNYUHy0iDe",
         },
       ],
     };
